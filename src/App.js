99c56a0d6c2c0f6bbbf32d8f803fa70ade9e3a43
dobosalpar10 <dobosalpar10@gmail.com>
2019-10-19 12:35:33 +0300

    material UI setup ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Button, MuiThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core';
+import muiTheme from './Theme/theme.json';
+
+const theme = createMuiTheme(muiTheme);
 
 const App = () => {
   return (
-    <div>
-      alma
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Button variant="contained" color="primary">
+        Button
+      </Button>
+    </MuiThemeProvider>
   );
 }
 
